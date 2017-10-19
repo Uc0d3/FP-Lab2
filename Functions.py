57b7170e03d7e964e0afde09b59783e0f232cbe4
Uc0d3 <unixcode.root@gmail.com>
2017-10-19 23:15:45 +0300

    Corrected is prime function(primes start from 2...) Added test for generate bigger prime

--- Functions.py
+++ Functions.py
@@ -3,7 +3,7 @@ from functools import reduce
 
 
 def is_prime(n):
-    if n <= 0:
+    if n < 2:
         return False
     return all(n % i for i in range(2, n // 2 + 1))
 
--- tests.py
+++ tests.py
@@ -3,7 +3,8 @@ from Functions import *
 
 
 def is_prime_test():
-    assert is_prime(1) is True
+    assert is_prime(2) is True
+    assert is_prime(1) is False
     assert is_prime(13) is True
     assert is_prime(7) is True
     assert is_prime(23) is True
@@ -14,5 +15,14 @@ def is_prime_test():
     print ("Is prime test passed")
 
 
+def gen_prime_bigger_test():
+    assert gen_prime_bigger(0) == 2
+    assert gen_prime_bigger(1) == 2
+    assert gen_prime_bigger(-10) == 2
+    assert gen_prime_bigger(7) == 11
+    print ("Gen prime bigger test passed")
+
+
 def run_tests():
     is_prime_test()
+    gen_prime_bigger_test()
